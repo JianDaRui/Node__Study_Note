02354cededd8a2270103a05240909232ed70dd8a
JianDaRui <17635269610@163.com>
2022-01-01 22:05:48 +0800

    feat: commander 库的基础使用

--- package/src/node基础/07-learn_cli/index.js
+++ package/src/node基础/07-learn_cli/index.js
@@ -3,10 +3,14 @@
 
 // commander.js 
 const program = require("commander")
-
+const helpOptions = require("./lib/core/help")
 console.log("coder 狂想曲")
 const version = require('./package.json').version
 program.version(version)
+// 可选信息
+helpOptions()
 
 // 对控制台参数进行解析
 program.parse(process.argv)
+
+console.log(program.dest)
---
+++ package/src/node基础/07-learn_cli/lib/core/help.js
@@ -0,0 +1,18 @@
+const program = require('commander')
+
+const helpOptions = () => {
+  // 增加自己的options
+  program.option('-C --coder', 'a coder cli')
+
+  program.option('-d --dest <dest>', 'a destination folder, 例如: -d /src/components')
+  program.option('-f --framework <framework>', 'your framework')
+
+  // 监听指令
+  program.on('--help', function() {
+    console.log("")
+    console.log("Help Info:")
+    console.log("...")
+  })
+}
+
+modules.exports = helpOptions
