66784bbfcd9273cc320b251e284e3d62c91c770b
JianDaRui <17635269610@163.com>
2022-01-02 12:16:16 +0800

    feat: 启动服务

--- package/src/node基础/07-learn_cli/lib/core/action.js
+++ package/src/node基础/07-learn_cli/lib/core/action.js
@@ -1,6 +1,7 @@
 // node 自有模块util promisify 可以将回调转为promise形式
 const { promisify } = require("util")
 const download = promisify(require("download-git-repo"));
+const open = require("open");
 const { vueRepo } = require("../config/repo-config")
 const { commandSpawn } = require("../utils/terminal")
 
@@ -15,10 +16,14 @@ const createProjectAction = async (project) => {
   // 1 通过 async 转promise 链式调用为 await
   await download(vueRepo, project, { clone: true })
   // 2 执行npm install
-  await commandSpawn("npm", ["install"], {cwd: `./${project}`})
-  // 3 运行npm run serve
+  const command = process.platform === "win32" ? "npm.cmd" : "npm"
+  await commandSpawn(command, ["install"], {cwd: `./${project}`})
 
+  // 3 运行npm run serve
+  // await commandSpawn(command, ["run", "serve"], {cwd: `./${project}`})
+  commandSpawn(command, ["run", "serve"], {cwd: `./${project}`})
   // 4 打开浏览器
+  open("http://localhost:8080/")
 }
 
 module.exports = {
